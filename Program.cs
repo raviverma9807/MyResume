e3c6247181b925c14e930183b64b33eb6efbfded
Ravi Verma <44615530+raviverma9807@users.noreply.github.com>
2025-08-23 23:22:08 +0530

    updated index

--- Program.cs
+++ Program.cs
@@ -1,20 +1,6 @@
-using Microsoft.Extensions.FileProviders;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Serve static files from project root
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(
-        Path.Combine(Directory.GetCurrentDirectory()))
-});
-
-// Serve index.html on all unmatched routes
-app.MapFallback(async context =>
-{
-    context.Response.ContentType = "text/html";
-    await context.Response.SendFileAsync(Path.Combine(Directory.GetCurrentDirectory(), "index.html"));
-});
-
+app.UseStaticFiles(); 
+app.MapFallbackToFile("index.html"); 
 app.Run();
